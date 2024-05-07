d8f29d0f196f6f69b2f5306649b64f1902e76ffd
LeAnhQuan91 <165478725+LeAnhQuan91@users.noreply.github.com>
2024-05-07 16:11:34 +0700

    change

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    { 
+        int M, N;
+        Console.WriteLine("Nhap so tien $ ban muon chuyen do:");
+        M = Convert.ToInt32(Console.ReadLine());
+        // chanring to money
+        N = M * 23000;
+
+        Console.WriteLine($"So Tien ban muon chuyen tu $ sang VND la:{M}");
+        Console.WriteLine($"So Tien VND la:{N}");
+
+    }
+}
